76ea42f78a1fa05ab187f4eea4270bcddd9f4d3d
littlekanine <elia.kopff3891@gmail.com>
2025-03-31 11:44:58 +0200

    WIP carrousel / add 3 projects

--- portefolio/src/app/component/cardProjectPhone/cardProjectPhone.js
+++ portefolio/src/app/component/cardProjectPhone/cardProjectPhone.js
@@ -1,11 +1,28 @@
-import './cardProjectPhone'
+"use client"
 
-const cardProjectPhone = () => {
+import { useState } from "react";
+import "./cardProjectPhone.scss";
+
+const CardProjectPhone = ({ nom, img, langages, website, gitHubSite, description }) => {
+    const [projectHover, setProjectHover] = useState(false);
+    const [projectClicked, setProjectClicked] = useState(false);
+
     return (
-        <div className='cardProject'>
-            
+        <div className="cardProject" onMouseEnter={() => setProjectHover(true)} onMouseLeave={() => setProjectHover(false)} onClick={() => setProjectClicked(!projectClicked)}>
+            <img src={img} alt={nom} className="projectImage" />
+            <h2>{nom}</h2>
+            <p>{description}</p>
+            <p>
+                <strong>Langages :</strong> {langages.join(", ")}
+            </p>
+            <a href={website} target="_blank" rel="noopener noreferrer">
+                Voir le site
+            </a>
+            <a href={gitHubSite} target="_blank" rel="noopener noreferrer">
+                GitHub
+            </a>
         </div>
-    )
-}
+    );
+};
 
-export default cardProjectPhone
+export default CardProjectPhone;
--- portefolio/src/app/component/carroussel/carroussel.js
+++ portefolio/src/app/component/carroussel/carroussel.js
@@ -1,22 +1,55 @@
+"use client"
+
 import "./carroussel.scss";
+import { useEffect, useState } from "react";
+import CardProjectPhone from "../cardProjectPhone/cardProjectPhone";
+import projectsData from "../../data/projectsData.json";
 
 const Carrousel = () => {
+    const totalProjects = projectsData.length;
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const visibleProjects = 3; // On affiche 3 projets à la fois
+
+    // Fonction pour avancer dans le carrousel
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % totalProjects);
+        }, 3000); // Change toutes les 3 secondes
+
+        return () => clearInterval(interval);
+    }, [totalProjects]);
+
+    // Fonction pour déterminer les projets visibles
+    const getVisibleProjects = () => {
+        let visible = [];
+        for (let i = 0; i < visibleProjects; i++) {
+            visible.push(projectsData[(currentIndex + i) % totalProjects]);
+        }
+        return visible;
+    };
 
     return (
-        <div>
-            <figure class="icon-cards mt-3">
-                <div class="icon-cards__content">
-                    <div class="icon-cards__item d-flex align-items-center justify-content-center">
-                        <span class="h1">🙂</span>
+        <div className="icon-cards mt-3">
+            <div className="icon-cards__content">
+                {getVisibleProjects().map((project, index) => (
+                    <div
+                        className="icon-cards__item"
+                        key={index}
+                        style={{
+                            transform: `rotateY(${index * 120}deg) translateZ(35vw)`,
+                        }}
+                    >
+                        <CardProjectPhone
+                            nom={project.nom}
+                            img={project.img}
+                            langages={project.langages}
+                            website={project.website}
+                            gitHubSite={project.gitHubSite}
+                            description={project.description}
+                        />
                     </div>
-                    <div class="icon-cards__item d-flex align-items-center justify-content-center">
-                        <span class="h1">😊</span>
-                    </div>
-                    <div class="icon-cards__item d-flex align-items-center justify-content-center">
-                        <span class="h1">😀</span>
-                    </div>
-                </div>
-            </figure>
+                ))}
+            </div>
         </div>
     );
 };
