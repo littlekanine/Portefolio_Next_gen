9d5e57979b8df1dea3967db173c8048acc5f57a2
littlekanine <elia.kopff3891@gmail.com>
2025-03-31 18:06:06 +0200

    wip Caroussel

--- portefolio/src/app/component/cardProjectPhone/cardProjectPhone.js
+++ portefolio/src/app/component/cardProjectPhone/cardProjectPhone.js
@@ -1,28 +1,33 @@
-"use client"
+'use client';
 
-import { useState } from "react";
-import "./cardProjectPhone.scss";
+import { useState } from 'react';
+import './cardProjectPhone.scss';
+import Image from 'next/image';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const CardProjectPhone = ({ nom, img, langages, website, gitHubSite, description }) => {
-    const [projectHover, setProjectHover] = useState(false);
-    const [projectClicked, setProjectClicked] = useState(false);
+	const [projectHover, setProjectHover] = useState(false);
 
-    return (
-        <div className="cardProject" onMouseEnter={() => setProjectHover(true)} onMouseLeave={() => setProjectHover(false)} onClick={() => setProjectClicked(!projectClicked)}>
-            <img src={img} alt={nom} className="projectImage" />
-            <h2>{nom}</h2>
-            <p>{description}</p>
-            <p>
-                <strong>Langages :</strong> {langages.join(", ")}
-            </p>
-            <a href={website} target="_blank" rel="noopener noreferrer">
-                Voir le site
-            </a>
-            <a href={gitHubSite} target="_blank" rel="noopener noreferrer">
-                GitHub
-            </a>
-        </div>
-    );
+	return (
+		<div className={`cardProject relative ${projectHover ? 'hover' : ''}`}>
+			<Image src={img} alt={nom} className="projectImage cover  border10" width={90} height={90} />
+			<AnimatePresence>
+				{/* <motion.div className="projectInfo">
+				<h2>{nom}</h2>
+				<p>{description}</p>
+				<p>
+					<strong>Langages :</strong> {langages.join(', ')}
+				</p>
+				<a href={website} target="_blank" rel="noopener noreferrer">
+					Voir le site
+				</a>
+				<a href={gitHubSite} target="_blank" rel="noopener noreferrer">
+					GitHub
+				</a>
+			</motion.div> */}
+			</AnimatePresence>
+		</div>
+	);
 };
 
 export default CardProjectPhone;
--- portefolio/src/app/component/carroussel/carroussel.js
+++ portefolio/src/app/component/carroussel/carroussel.js
@@ -1,57 +1,52 @@
-"use client"
+'use client';
 
-import "./carroussel.scss";
-import { useEffect, useState } from "react";
-import CardProjectPhone from "../cardProjectPhone/cardProjectPhone";
-import projectsData from "../../data/projectsData.json";
+import './carroussel.scss';
+import { useEffect, useState } from 'react';
+import CardProjectPhone from '../cardProjectPhone/cardProjectPhone';
+import projectsData from '../../data/projectsData.json';
 
 const Carrousel = () => {
-    const totalProjects = projectsData.length;
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const visibleProjects = 3; // On affiche 3 projets à la fois
+	const totalProjects = projectsData.length;
+	const [currentIndex, setCurrentIndex] = useState(0);
 
-    // Fonction pour avancer dans le carrousel
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % totalProjects);
-        }, 3000); // Change toutes les 3 secondes
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
+		}, 3000); // Change toutes les 3 secondes
 
-        return () => clearInterval(interval);
-    }, [totalProjects]);
-
-    // Fonction pour déterminer les projets visibles
-    const getVisibleProjects = () => {
-        let visible = [];
-        for (let i = 0; i < visibleProjects; i++) {
-            visible.push(projectsData[(currentIndex + i) % totalProjects]);
-        }
-        return visible;
-    };
-
-    return (
-        <div className="icon-cards mt-3">
-            <div className="icon-cards__content">
-                {getVisibleProjects().map((project, index) => (
-                    <div
-                        className="icon-cards__item"
-                        key={index}
-                        style={{
-                            transform: `rotateY(${index * 120}deg) translateZ(35vw)`,
-                        }}
-                    >
-                        <CardProjectPhone
-                            nom={project.nom}
-                            img={project.img}
-                            langages={project.langages}
-                            website={project.website}
-                            gitHubSite={project.gitHubSite}
-                            description={project.description}
-                        />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+		return () => clearInterval(interval);
+	}, [totalProjects]);
+	return (
+		<div className="flex center column">
+			<div className="icon-cards mt-30 flex column">
+				<div className="icon-cards__content flex column">
+					{projectsData.map((project, index) => (
+						<div
+							className="flex icon-cards__item"
+							key={index}
+							style={{
+								transform: `rotateY(${index * 120}deg) translateZ(35vw)`,
+							}}
+						>
+							<CardProjectPhone
+								nom={project.nom}
+								img={project.img}
+								langages={project.langages}
+								website={project.website}
+								gitHubSite={project.gitHubSite}
+								description={project.description}
+							/>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="flex dot-container center align-center gap10">
+				{projectsData.map((_, i) => (
+					<div key={i} className={`flex bullet ${currentIndex === i ? 'vertClaire' : 'vertFonce'}`}></div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Carrousel;
