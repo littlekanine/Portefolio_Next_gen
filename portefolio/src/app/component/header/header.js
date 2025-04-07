1d59d1f926e22e2fbd2c106ceb92acb7ee775089
littlekanine <elia.kopff3891@gmail.com>
2025-04-07 20:08:59 +0200

    WORK on style phone

--- portefolio/src/app/component/header/header.js
+++ portefolio/src/app/component/header/header.js
@@ -1,15 +1,17 @@
-import "./header.scss";
+import './header.scss';
 
 const Header = () => {
-    return (
-        <div className="flex center align-center gap20 widthFull ">
-            <div className="flex center align-center gap20 width50 header ">
-                <button>About</button>
-                <button>projects</button>
-                <button>Contact</button>
-            </div>
-        </div>
-    );
+	return (
+		<div className="flex center align-center widthFull container">
+			<div className="flex center align-center gap20 containerHeader">
+				<div className="flex center align-center gap20 header">
+					<button>About</button>
+					<button>projects</button>
+					<button>Contact</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
