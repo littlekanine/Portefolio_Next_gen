124432dc7b547a7d7f07540a55108ef2c8ed54f5
littlekanine <elia.kopff3891@gmail.com>
2025-04-07 13:54:10 +0200

    WIP

--- portefolio/src/app/component/header/header.js
+++ portefolio/src/app/component/header/header.js
@@ -2,10 +2,12 @@ import "./header.scss";
 
 const Header = () => {
     return (
-        <div className="flex center align-center gap20 header">
-            <button>About</button>
-            <button>projects</button>
-            <button>Contact</button>
+        <div className="flex center align-center gap20 widthFull ">
+            <div className="flex center align-center gap20 width50 header ">
+                <button>About</button>
+                <button>projects</button>
+                <button>Contact</button>
+            </div>
         </div>
     );
 };
--- portefolio/src/app/pages/homePage/homePage.js
+++ portefolio/src/app/pages/homePage/homePage.js
@@ -4,7 +4,6 @@ import { faSquareGithub, faLinkedin, faJs } from "@fortawesome/free-brands-svg-i
 import { faSwatchbook, faCode, faGlobe, faEye } from "@fortawesome/free-solid-svg-icons";
 import Card from "@/app/component/cards/card";
 import Carrousel from "@/app/component/carroussel/carroussel";
-import Image from "next/image";
 import FormContact from "@/app/component/formcontact/formContact";
 
 const HomePage = () => {
@@ -14,49 +13,51 @@ const HomePage = () => {
                 <div className="width75 flex center align-center relative column">
                     <div className="flex center align-center relative column">
                         <h1 className="relative">
-                            <div className="widthAuto flex row elia shadow">&lt; Elia Kopff</div>
+                            <div className="widthAuto flex row elia shadow"> Elia Kopff</div>
                             <br />
-                            <span className="dev shadow">Developer Web /&gt;</span>
+                            <span className="dev shadow">Developer Web</span>
                         </h1>
                         <FontAwesomeIcon icon={faSquareGithub} className="custom-icon git absolute" />
                         <FontAwesomeIcon icon={faLinkedin} className="custom-icon linkedin absolute" />
                     </div>
                 </div>
-                <div className="photo width25 relative flex center align-center">
+                {/* <div className="photo width25 relative flex center align-center">
                     <div className="ribbon absolute">
-                        <span>Réactif</span>
+                        <span>A votre écoute</span>
                     </div>
-                    <div className=" flex center align-center cadre"></div>
-                </div>
+                </div> */}
             </div>
-            <div className="what widthFull flex  column gap10">
-                <h2 className="flex center shadow">&lt; What I do /&gt;</h2>
-                <div className="flex center align-center row gap10">
-                    <Card iconSrc={faSwatchbook} title={"UX/UI design"} text={"Création du design et adaptation en fonction des besoins client."} />
-                    <Card iconSrc={faCode} title={"Development"} text={"Intégration du design, ajout des fonctionnalités"} />
+            <div className="widthFull flex center align-center column gap10 vertFonce">
+                <div className="about flex center widthFull padding10 column">
+                    <div className="heightFull flex center column relative">
+                        <h2 className="flex center shadow absolute">About </h2>
+                        <div className="flex center align-center widthFull">
+                            <div className=" flex center align-center column gap10">
+                                <p className="text">Je suis un développeur web passionné par la création de sites internet et d'applications web. J'aime relever des défis techniques et trouver des solutions innovantes pour répondre aux besoins de mes clients.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex center align-center row gap10">
-                    <Card iconSrc={faGlobe} title={"SEO"} text={"Mise en place du référencement du site, ainsi que son accessibilité"} />
-                    <Card iconSrc={faEye} title={"Support"} text={"Accompagnement au delà du projet, résolution des problémes."} />
+                <div className="what widthFull flex  column gap10">
+                    <h2 className="flex center shadow"> What I do</h2>
+                    <div className="flex center align-center row gap10">
+                        <Card iconSrc={faSwatchbook} title={"UX/UI design"} text={"Création du design et adaptation en fonction des besoins client."} />
+                        <Card iconSrc={faCode} title={"Development"} text={"Intégration du design, ajout des fonctionnalités"} />
+                    </div>
+                    <div className="flex center align-center row gap10">
+                        <Card iconSrc={faGlobe} title={"SEO"} text={"Mise en place du référencement du site, ainsi que son accessibilité"} />
+                        <Card iconSrc={faEye} title={"Support"} text={"Accompagnement au delà du projet, résolution des problémes."} />
+                    </div>
                 </div>
             </div>
             <div className="projects widthFull column gap10">
-                <h2 className="flex center title shadow">&lt; Projects /&gt;</h2>
+                <h2 className="flex center title shadow"> Projects</h2>
                 <div className="flex center align-center">
                     <Carrousel />
                 </div>
             </div>
-            {/* <div className="about flex center widthFull padding10 column">
-				<div className="heightFull flex center column">
-					<h2 className="flex center shadow">&lt;About /&gt;</h2>
-					<div className="flex center align-center row">
-						<FontAwesomeIcon icon={faJs} className="js" />
-						<Image src={'/nextjs-icon.svg'} width={30} height={30} className="next" />
-					</div>
-				</div>
-			</div> */}
             <div className="contact widthFull flex center column gap10">
-                <h2 className="flex center title shadow">&lt; Contact /&gt;</h2>
+                <h2 className="flex center title shadow"> Contact </h2>
                 <div className="flex center align-center widthFull">
                     <FormContact />
                 </div>
