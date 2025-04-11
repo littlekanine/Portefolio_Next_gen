ae545ec97df89d26e325a410c03a361c43898690
littlekanine <elia.kopff3891@gmail.com>
2025-04-11 10:29:02 +0200

    Style project OK

--- portefolio/src/app/component/cardProjectPhone/cardProjectPhone.js
+++ portefolio/src/app/component/cardProjectPhone/cardProjectPhone.js
@@ -1,86 +1,86 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from 'react';
-import './cardProjectPhone.scss';
-import Image from 'next/image';
-import { motion, AnimatePresence } from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import IconMap from '../iconMap/iconMap';
+// import { useEffect, useState } from 'react';
+// import './cardProjectPhone.scss';
+// import Image from 'next/image';
+// import { motion, AnimatePresence } from 'framer-motion';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPlus } from '@fortawesome/free-solid-svg-icons';
+// import IconMap from '../iconMap/iconMap';
 
-const CardProjectPhone = ({ nom, img, langages, website, gitHubSite, description, isOpen, onClick, onClose }) => {
-	const [plusClicked, setPlusClicked] = useState(false);
+// const CardProjectPhone = ({ nom, img, langages, website, gitHubSite, description, isOpen, onClick, onClose }) => {
+// 	const [plusClicked, setPlusClicked] = useState(false);
 
-	useEffect(() => {
-		if (!isOpen) {
-			setPlusClicked(false);
-		}
-	}, [isOpen]);
+// 	useEffect(() => {
+// 		if (!isOpen) {
+// 			setPlusClicked(false);
+// 		}
+// 	}, [isOpen]);
 
-	const handleClickPlus = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-		setPlusClicked((prev) => !prev);
-	};
+// 	const handleClickPlus = (e) => {
+// 		e.preventDefault();
+// 		e.stopPropagation();
+// 		setPlusClicked((prev) => !prev);
+// 	};
 
-	return (
-		<div className={`cardContainer flex column relative ${plusClicked && isOpen ? 'extraOpened' : ''}`}>
-			<div onClick={onClick} className="cardProject overflow-none relative column gap10">
-				<Image src={img} alt={nom} className={`projetImage cover border10 ${isOpen ? 'sombre' : ''} `} width={90} height={90} />
-				<AnimatePresence>
-					{isOpen && (
-						<motion.div
-							className="projectInfo relative"
-							initial={{ opacity: 0, y: 100 }}
-							animate={{ opacity: 1, y: 0 }}
-							exit={{ opacity: 0, y: 100 }}
-							transition={{ duration: 0.2 }}
-						>
-							<h2 className="flex center align-center">{nom}</h2>
-							<motion.div className="description relative" transition={{ duration: 0.2 }}>
-								<div className="descriptionContent">
-									<p>{description}</p>
-								</div>
-								<button className="iconButton" onClick={handleClickPlus}>
-									<FontAwesomeIcon icon={faPlus} className="icon" />
-								</button>
-							</motion.div>
-						</motion.div>
-					)}
-				</AnimatePresence>
-			</div>
+// 	return (
+// 		<div className={`cardContainer flex column relative ${plusClicked && isOpen ? 'extraOpened' : ''}`}>
+// 			<div onClick={onClick} className="cardProject overflow-none relative column gap10">
+// 				<Image src={img} alt={nom} className={`projetImage cover border10 ${isOpen ? 'sombre' : ''} `} width={90} height={90} />
+// 				<AnimatePresence>
+// 					{isOpen && (
+// 						<motion.div
+// 							className="projectInfo relative"
+// 							initial={{ opacity: 0, y: 100 }}
+// 							animate={{ opacity: 1, y: 0 }}
+// 							exit={{ opacity: 0, y: 100 }}
+// 							transition={{ duration: 0.2 }}
+// 						>
+// 							<h2 className="flex center align-center">{nom}</h2>
+// 							<motion.div className="description relative" transition={{ duration: 0.2 }}>
+// 								<div className="descriptionContent">
+// 									<p>{description}</p>
+// 								</div>
+// 								<button className="iconButton" onClick={handleClickPlus}>
+// 									<FontAwesomeIcon icon={faPlus} className="icon" />
+// 								</button>
+// 							</motion.div>
+// 						</motion.div>
+// 					)}
+// 				</AnimatePresence>
+// 			</div>
 
-			<AnimatePresence>
-				{plusClicked && isOpen && (
-					<motion.div
-						className="extraInfo flex center align-center gap10 absolute"
-						initial={{ opacity: 0, y: -10 }}
-						animate={{ opacity: 1, y: 0 }}
-						exit={{ opacity: 0, y: -10 }}
-						transition={{ duration: 0.2 }}
-					>
-						<div className="flex row space-between widthFull">
-							<div className="flex row align-center gap10">
-								<a href={website} target="_blank" rel="noopener noreferrer" className="flex link">
-									<FontAwesomeIcon icon={IconMap.globe} className="icon" />
-								</a>
-								<a href={gitHubSite} target="_blank" rel="noopener noreferrer" className="flex link">
-									<FontAwesomeIcon icon={IconMap.gitHub} className="icon" />
-								</a>
-							</div>
-							<div className="flex row align-center gap10">
-								{langages.map((langage) => (
-									<div className="flex center align-center" key={langage}>
-										<FontAwesomeIcon icon={IconMap[langage]} className="icon" />
-									</div>
-								))}
-							</div>
-						</div>
-					</motion.div>
-				)}
-			</AnimatePresence>
-		</div>
-	);
-};
+// 			<AnimatePresence>
+// 				{plusClicked && isOpen && (
+// 					<motion.div
+// 						className="extraInfo flex center align-center gap10 absolute"
+// 						initial={{ opacity: 0, y: -10 }}
+// 						animate={{ opacity: 1, y: 0 }}
+// 						exit={{ opacity: 0, y: -10 }}
+// 						transition={{ duration: 0.2 }}
+// 					>
+// 						<div className="flex row space-between widthFull">
+// 							<div className="flex row align-center gap10">
+// 								<a href={website} target="_blank" rel="noopener noreferrer" className="flex link">
+// 									<FontAwesomeIcon icon={IconMap.globe} className="icon" />
+// 								</a>
+// 								<a href={gitHubSite} target="_blank" rel="noopener noreferrer" className="flex link">
+// 									<FontAwesomeIcon icon={IconMap.gitHub} className="icon" />
+// 								</a>
+// 							</div>
+// 							<div className="flex row align-center gap10">
+// 								{langages.map((langage) => (
+// 									<div className="flex center align-center" key={langage}>
+// 										<FontAwesomeIcon icon={IconMap[langage]} className="icon" />
+// 									</div>
+// 								))}
+// 							</div>
+// 						</div>
+// 					</motion.div>
+// 				)}
+// 			</AnimatePresence>
+// 		</div>
+// 	);
+// };
 
-export default CardProjectPhone;
+// export default CardProjectPhone;
--- portefolio/src/app/component/projectSection/projectSection.js
+++ portefolio/src/app/component/projectSection/projectSection.js
@@ -3,6 +3,9 @@
 import React, { useState } from "react";
 import projects from "../../data/projectsData.json"; // Assurez-vous que le fichier JSON est bien à cet emplacement
 import "./projectSection.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { GlobeIcon, GithubIcon } from "lucide-react";
+import IconMap from "../iconMap/iconMap";
 
 export const ProjectsSection = () => {
     const [openedProjectIndex, setOpenedProjectIndex] = useState(null);
@@ -13,18 +16,33 @@ export const ProjectsSection = () => {
                 <h1 className="projects__title">Projects</h1>
                 <div className="projects__intro-list center">
                     {projects.map((project, index) => (
-                        <div key={project.id} className={`projects__card ${openedProjectIndex === index ? "projects__card--opened" : ""}`} onClick={() => setOpenedProjectIndex(openedProjectIndex === index ? null : index)}>
-                            <div className="projects__card-img">
+                        <div key={project.id} className={`projects__card ${openedProjectIndex === index ? "projects__card--opened" : ""} relative`} onClick={() => setOpenedProjectIndex(openedProjectIndex === index ? null : index)}>
+                            <div className="projects__card-img flex">
                                 <img src={project.img} alt={project.nom} className="projects__card-image" />
                             </div>
-
-                            {/* Affichage conditionnel des infos quand le projet est sélectionné */}
-                            {openedProjectIndex === index && (
-                                <div className="projects__card-info">
-                                    <h3 className="projects__card-name">{project.nom}</h3>
+                            <div className="projects__card-info cache column">
+                                <div className="projects__card-info-content flex column gap10 heightFull space-between">
+                                    <h2 className="projects__card-name">{project.nom}</h2>
                                     <p className="projects__card-description">{project.description[0]}</p>
+                                    <div className="flex row space-between widthFull center align-center">
+                                        <div className="flex row gap10">
+                                            <a href={project.website} target="_blank" rel="noopener noreferrer" className="flex link">
+                                                <GlobeIcon size={20} />
+                                            </a>
+                                            <a href={project.gitHubSite} target="_blank" rel="noopener noreferrer" className="flex link">
+                                                <GithubIcon size={20} />
+                                            </a>
+                                        </div>
+                                        <div className="flex row gap10">
+                                            {project.langages.map((langage, index) => (
+                                                <span key={langage} className="projects__card-techno">
+                                                    <FontAwesomeIcon icon={IconMap[langage]} className="icon" />
+                                                </span>
+                                            ))}
+                                        </div>
+                                    </div>
                                 </div>
-                            )}
+                            </div>
                         </div>
                     ))}
                 </div>
